2a2316b703f60a65f4ba65c469675e9bc0162df9
bobbi-SMR <bobbi_fox@harvard.edu>
2018-07-12 15:23:52 -0400

    s3 support class

---
+++ s3_support/s3.py
@@ -0,0 +1,62 @@
+from os.path import exists
+import yaml
+from boltons.dictutils import OMD
+import boto3
+from botocore.exceptions import ClientError
+import logging
+from pprint import pprint
+class S3:
+    """ This class supports S3 activity on a SINGLE bucket.  
+    It raises a ValueError if the credentails are invalid, or the bucket hasn't been created
+    """
+    def __init__(self, **kwargs):
+        access = kwargs.get('accesskey', None)
+        secret = kwargs.get('secret', None)
+        bucket = kwargs.get('bucket', '')
+        configpath = kwargs.get('configpath', None)
+        omd = OMD()
+        omd.update({
+            'accesskey': access,
+            'secret': secret,
+            'bucket': bucket
+        })
+        if configpath is not None and exists(configpath):
+            with open(configpath, 'r') as f:
+                omd.update_extend(yaml.safe_load(f))
+        self.access = omd.get('accesskey', access)
+        self.secret = omd.get('secret', secret)
+        bucket_name = omd.get('bucket',bucket)
+        cd = None
+        try:
+            self.s3 = boto3.client('s3', aws_access_key_id=self.access, aws_secret_access_key=self.secret)
+            self.bucket = boto3.resource('s3', aws_access_key_id=self.access, aws_secret_access_key=self.secret).Bucket(bucket_name)
+            cd = self.bucket.creation_date
+            # make sure you actually have a bucket
+        except ClientError as e:
+            raise ValueError(e.response['Error']['Message'])
+        if cd is None:
+            raise ValueError("This bucket [" + self.bucket.name + "] does not exist") 
+        logging.getLogger("connectionpool.py").setLevel(logging.WARNING)
+        logging.getLogger("connectionpool").setLevel(logging.WARNING)
+    def __str__(self):
+        if self.bucket:
+            return "S3 bucket " + self.bucket.name
+        else:
+            return "S3 with no bucket!"
+    
+    def download(self,key, filepath):
+        self.bucket.download_file(key, filepath)
+
+    def get_object_head(self,key):
+        head = None
+        try:
+            head = self.s3.head_object(Bucket=self.bucket.name, Key=key)
+        except ClientError as e:
+            if e.response['Error']['Code'] != '404':
+                raise
+        return head
+    def remove(self, key):
+        self.s3.delete_object(Bucket = self.bucket.name, Key = key)
+
+    def upload(self,filepath, key):
+        self.bucket.upload_file(filepath, key)
