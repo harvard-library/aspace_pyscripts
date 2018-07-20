aeb128331cb7ff70864e336e0ee479a23fe3678f
bobbi-SMR <bobbi_fox@harvard.edu>
2018-07-20 13:35:43 -0400

    Use the RIGHT libraries!

--- bin/install_libraries.sh
+++ bin/install_libraries.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+pip3 install --user ArchivesSnake
 pip3 install --user boto3
-pip3 install --user urllib2
-pip3 install --user simplejason
 pip3 install --user SolrClient
+pip3 install --user python-magic
+
 
