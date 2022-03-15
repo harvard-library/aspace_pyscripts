d03ef2e32d2c6da173667782d855f248944df714
Dave Mayo <dave_mayo@harvard.edu>
2022-03-15 09:13:55 -0400

    Check for PID file in node rather than in script

--- cron.js
+++ cron.js
@@ -1,14 +1,17 @@
 const Cron = require("croner");
+const fs = require("fs");
 const {spawn} = require("child_process");
 
 Cron("* */20 * * * *", {}, ()=> {
   console.log("running pdfStorer.py");
   try {
-    subprocess = spawn("/aspace_pyscripts/pdfStorer.py",
-                       ["-t", "dave_mayo@harvard.edu",
-                        "-f", "dave_mayo@harvard.edu"]);
-    subprocess.stdout.on('data', (data) => { console.log(data) });
-    subprocess.stderr.on('data', (data) => { console.log("ERR: " + data) });
+    if ( !fs.existsSync("/aspace_pyscripts/pdfstorerdaemon.pid") ) {
+      subprocess = spawn("/aspace_pyscripts/pdfStorer.py",
+                         ["-t", "dave_mayo@harvard.edu",
+                          "-f", "dave_mayo@harvard.edu"]);
+      subprocess.stdout.on('data', (data) => { console.log(data) });
+      subprocess.stderr.on('data', (data) => { console.log("ERR: " + data) });
+    }
   }
   catch (e) {
     console.log(e);
